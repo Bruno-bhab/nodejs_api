702b3e88a4323404fd1007f631e1980ae6eb0875
bruno-bhab <bruno.henrique@beedoo.com.br>
2022-08-01 00:51:07 -0300

    chapterII>3/1

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes'
+
 const app = express()
+app.use(express.json())
 
+//Routes
+app.use("/categories", categoriesRoutes)
 
-app.get("/", (request, response) => {
-    return response.send("Hello world!")
-
-})
-
-
+//Server start
 app.listen(3333, () => console.log("Server is running!"))
